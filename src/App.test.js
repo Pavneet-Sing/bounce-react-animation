4501b2c8566d4b0250be76f863e81a8010073cd8
Pavneet-Sing <dev.pavneet@gmail.com>
2020-07-29 16:58:56 +0530

    implement view bound verify test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react/';
 import App from './App';
+import '@testing-library/jest-dom/extend-expect'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('verify football bounds before and after aniamtion', () => {
+  const { container } = render(<App />);
+  const circleElement = container.getElementsByClassName('circle')[0];
+
+  expect(elementInViewport(circleElement)).toBeTruthy();
+  fireEvent.click(circleElement);
+  expect(circleElement).toBeInTheDocument();
+
+  expect(elementInViewport(circleElement)).toBeTruthy();
 });
+
+function elementInViewport(el) {
+  var top = el.offsetTop;
+  var left = el.offsetLeft;
+  var width = el.offsetWidth;
+  var height = el.offsetHeight;
+
+  while (el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+    left += el.offsetLeft;
+  }
+
+  return (
+    top >= window.pageYOffset &&
+    left >= window.pageXOffset &&
+    (top + height) <= (window.pageYOffset + window.innerHeight) &&
+    (left + width) <= (window.pageXOffset + window.innerWidth)
+  );
+}
